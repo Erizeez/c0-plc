a9b29d8bcd05509a0c406662337ec7fb85557b41
Erizeez <820540373@qq.com>
2020-12-04 17:29:11 +0800

    h

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -24,6 +24,7 @@ public class App {
     public static void main(String[] args) throws CompileError, TokenizeError, IOException {
         InputStream input = null;
         String outFile = null;
+        String infile = null;
         for(int i = 0; i < args.length; i++){
             if(args[i].equals("-l")){
                 input = new FileInputStream(args[i + 1]);
@@ -41,6 +42,19 @@ public class App {
         scanner = new Scanner(input);
         var iter = new StringIter(scanner);
         var tokenizer = tokenize(iter);
+        OutputStream outPutStream;
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(new File(infile)));//构造一个BufferedReader类来读取文件
+            String s = null;
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+                result.append(System.lineSeparator()+s);
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(result.toString());
 
 //        var tokens = new ArrayList<Token>();
 //        try {
